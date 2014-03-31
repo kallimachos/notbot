72391453c7ecdc19e1c4aca2f79a485ce83e5b7b
Brian Moss <bmoss@redhat.com>
2014-03-31 17:45:20 +1000

    Account for invalid commands and add more responses to voice.yaml

--- notbot.py
+++ notbot.py
@@ -7,7 +7,7 @@ from random import choice
 
 HOST = "irc.bne.redhat.com"
 PORT = 6667
-CHANNEL = '#test4notbot'
+CHANNEL = '#test4bot'
 NICK = "notbot"
 IDENT = "notbot"
 REALNAME = "NotBot"
@@ -23,14 +23,19 @@ s.send("JOIN %s\r\n" % CHANNEL)
 s.send("PRIVMSG %s :Hi everybody!  No way I'm not a notbot.\r\n" % CHANNEL)
 
 while True:
-   data = s.recv (1024)
-   if data.find('PING') != -1:
-      s.send('PONG ' + data.split()[1] + '\r\n')
-   elif data.find(NICK):
-      for command in voice:
-         if data.find(command) != -1:
-            response = voice[command][0]
+    data = s.recv (1024)
+    if data.find('PING') != -1:
+        s.send('PONG ' + data.split()[1] + '\r\n')
+    elif data.find(NICK) != -1 and data.find('PRIVMSG') != -1:
+        print data
+        found = False
+        for command in voice:
+            if data.find(command) != -1:
+                found = True
+                response = choice(voice[command])
+                s.send('PRIVMSG %s :%s\r\n' % (CHANNEL, response))
+                if command == 'quit' or command == 'bye':
+                    s.close()
+        if found == False: # need to account for invalid commands without spamming the list
+            response = choice(voice['badcommand'])
             s.send('PRIVMSG %s :%s\r\n' % (CHANNEL, response))
-   
-   # need to account for invalid commands
-   # s.send('PRIVMSG %s :Unknown command\r\n' % (CHANNEL))
